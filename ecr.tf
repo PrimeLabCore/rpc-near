274c3bf6101fd1ad72aeb1651fc7a03843f65a27
Mark-irozuru <mark.irozuru@primelab.io>
2022-04-04 19:07:44 +0100

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "near_rpc_for_primelab" {
-  name                 = "near-rpc-for-primelab/${var.ecr_subrepositories[count.index]}"
+  name                 = "near-rpc-for-primelab/${var.ecr_subrepositories}"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
