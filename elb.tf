8f1f89dcaef79e724dcd089760afb025919e50ed
Mark-irozuru <mark.irozuru@primelab.io>
2022-04-04 19:42:57 +0100

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -27,7 +27,7 @@ resource "aws_lb_target_group" "primelab_tg_24567" {
 
 resource "aws_lb_listener" "primelab_node_listener_3030" {
   for_each          = var.primelab_nodes
-  load_balancer_arn = aws_lb.primelab_nlb.arn
+  load_balancer_arn = aws_lb.primelab_nlb[each.key].arn
   port              = 3030
   protocol          = "TCP"
 
@@ -39,12 +39,12 @@ resource "aws_lb_listener" "primelab_node_listener_3030" {
 
 resource "aws_lb_listener" "primelab_node_listener_24567" {
   for_each          = var.primelab_nodes
-  load_balancer_arn = aws_lb.primelab_nlb.arn
+  load_balancer_arn = aws_lb.primelab_nlb[each.key].arn
   port              = 24567
   protocol          = "TCP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.primelab_tg_24567.arn
+    target_group_arn = aws_lb_target_group.primelab_tg_24567[each.key].arn
   }
 }
